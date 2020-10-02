2d0c4fbd79d9fad35375a8868d5011b2478c7537
iZeQure <iZeQure@users.noreply.github.com>
2020-10-02 22:36:48 +0200

    [Bot Project Added] - 0.6.0

---
+++ TQC-Development/DiscordBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordBot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
