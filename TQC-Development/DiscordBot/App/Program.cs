827b5ebac6b93973dc2b1e4e3f798274fc51c64a
iZeQure <iZeQure@users.noreply.github.com>
2020-10-27 23:21:37 +0100

    [Bot Changes] - 1.4.1
    Fixed an Issue where the Bot couldn't verify the user who reacted to a message. Now able to download the users into the cache and get the cache for the socket reaction.

--- TQC-Development/DiscordBot/App/Program.cs
+++ TQC-Development/DiscordBot/App/Program.cs
@@ -12,11 +12,11 @@ namespace DiscordBot
 {
     public class Program
     {
+        private const string DiscordToken = "Insert Token Here";
+
         public static void Main(string[] args)
             => new Program()
-            .MainAsync()
-            .GetAwaiter()
-            .GetResult();
+                .InitBotAsync().GetAwaiter().GetResult();
 
         private DiscordSocketClient _client;
         private DiscordSocketConfig _config;
@@ -25,75 +25,94 @@ namespace DiscordBot
         private INotifiable _notifiable;
         private ILoggable _loggable;
 
-        public async Task MainAsync()
+        public async Task InitBotAsync()
         {
-            // Configuration.
-            _config = new DiscordSocketConfig()                                         // Set DiscordSocket Configuration.
+            try
             {
-                MessageCacheSize = 100,
-                LogLevel = LogSeverity.Info,
-                ConnectionTimeout = 15,
-                DefaultRetryMode = RetryMode.AlwaysRetry,
-                HandlerTimeout = 15
-            };
+                // Configuration.
+                _config = new DiscordSocketConfig()                                         // Set DiscordSocket Configuration.
+                {
+                    MessageCacheSize = 100,
+                    LogLevel = LogSeverity.Info,
+                    DefaultRetryMode = RetryMode.RetryTimeouts,
+                    HandlerTimeout = 15
+                };
 
-            // Client assignment.
-            _client = new DiscordSocketClient(_config);                                 // Initialize new Discord Client with Configuration.
+                // Client assignment.
+                _client = new DiscordSocketClient(_config);                                 // Initialize new Discord Client with Configuration.
 
-            // Service Injections.
-            _dataService = new DataService();                                           // Initialize new Data Service.
-            _loggable = new LogService();                                               // Initialize new Log Service.
-            _notifiable = new NotificationService(_client, _loggable, _dataService);    // Inject Notification Service.
+                // Service Injections.
+                _dataService = new DataService();                                           // Initialize new Data Service.
+                _loggable = new LogService();                                               // Initialize new Log Service.
+                _notifiable = new NotificationService(_client, _loggable, _dataService);    // Inject Notification Service.
 
-            //_service = new ReactionService(_client);                                  // V1 - Reaction Service.
-            _reactable = new ReactionServiceV2(_notifiable, _loggable, _dataService);   // V2 - Reaction Service with Dependencies.
+                //_service = new ReactionService(_client);                                  // V1 - Reaction Service.
+                _reactable = new ReactionServiceV2(_notifiable, _loggable, _dataService);   // V2 - Reaction Service with Dependencies.
 
-            //_reactable = new TestService(_notifiable, _loggable);                     // Debugging for testing new service.
+                //_reactable = new TestService(_notifiable, _loggable);                     // Debugging for testing new service.
 
-            // Events.
-            _client.ReactionAdded += ReactionAdded;                                     // Bind ReactionAdded Event.
-            _client.Log += _loggable.Log;                                               // Bind Log Event.
-            _client.Ready += () =>                                                      // Bind ON Ready Event.
+                // Events.
+                _client.ReactionAdded += ReactionAdded;                                     // Bind ReactionAdded Event.
+                _client.Log += _loggable.Log;                                               // Bind Log Event.
+                _client.Ready += () =>                                                      // Bind ON Ready Event.
+                {
+                    return Task.CompletedTask;
+                };
+
+                await _client.LoginAsync(TokenType.Bot,                                     // Login with Bot.
+                    DiscordToken);
+
+                await _client.StartAsync();                                                 // Start Service.
+
+                // Block this task until the program is closed.
+                await Task.Delay(-1);                                                       // Await Task for Indefinitely. (Or program is closed)
+            }
+            catch (Exception ex)
             {
-                return Task.CompletedTask;
-            };
-
-            await _client.LoginAsync(TokenType.Bot, "Enter Token Here");                // Login with Bot.
-
-            await _client.StartAsync();                                                 // Start Service.
-
-            // Block this task until the program is closed.
-            await Task.Delay(-1);                                                       // Await Task for Indefinitely. (Or program is closed)
+                await _loggable.Log(
+                    new LogMessage(
+                        severity: LogSeverity.Error,
+                        source: "System Exception",
+                        message: ex.ToString(),
+                        exception: ex)
+                    );
+            }
         }
 
-        private async Task ReactionAdded(Cacheable<IUserMessage, ulong> userMessage, ISocketMessageChannel socketMessageChannel, SocketReaction socketReaction)
+        private Task ReactionAdded(Cacheable<IUserMessage, ulong> userMessage, ISocketMessageChannel socketMessageChannel, SocketReaction socketReaction)
         {
-            // Check if the reaction is from the corrects channels. (e.g. Steam, Xbox, Psn)
-            if (
-                socketReaction.Channel.Id == 765277945194348544 ||
-                socketReaction.Channel.Id == 765277969278042132 ||
-                socketReaction.Channel.Id == 765277993454534667)
+            _ = Task.Run(async () =>
             {
-                var message = await userMessage.GetOrDownloadAsync();
+                var user = await socketReaction.Channel.GetUserAsync(socketReaction.UserId) as SocketGuildUser;
 
-                //Check if the admin tries to react.
-                if (socketReaction.User.Value is SocketGuildUser user)
+                // Check if the reaction is from the corrects channels. (e.g. Steam, Xbox, Psn)
+                if (
+                    socketReaction.Channel.Id == 765277945194348544 ||
+                    socketReaction.Channel.Id == 765277969278042132 ||
+                    socketReaction.Channel.Id == 765277993454534667)
                 {
-                    // Check if the user is from the leadership role, or is the owner of the guild.
-                    if (user.Roles.Any(r => r.Name == "Leadership") || user.Id == 275816977631805451)
-                        await _loggable.Log(
-                            new LogMessage(
-                                LogSeverity.Info, 
-                                "Clan Application", $"Admin <{user.Id}> added reaction emote <{socketReaction.Emote.Name}> to message"));
-                    else
+                    var message = await userMessage.GetOrDownloadAsync();
+
+                    //Check if the admin tries to react.
+                    if (user is SocketGuildUser test)
                     {
-                        // Assign user to clan.
-                        await _reactable.ClanApplicationAsync(
-                            message, 
-                            socketReaction);
+                        // Check if the user is from the leadership role, or is the owner of the guild.
+                        if (test.Roles.Any(r => r.Name == "Leadership") || test.Id == 275816977631805451)
+                            await _loggable.Log(
+                                new LogMessage(
+                                    LogSeverity.Info,
+                                    "Clan Application", $"Admin <{test.Id}> added reaction emote <{socketReaction.Emote.Name}> to message"));
+                        else
+                        {
+                            // Assign user to clan.
+                            await _reactable.ClanApplicationAsync(
+                                message,
+                                socketReaction);
+                        }
                     }
                 }
-            }
+            });
+            return Task.CompletedTask;
         }
     }
 }
