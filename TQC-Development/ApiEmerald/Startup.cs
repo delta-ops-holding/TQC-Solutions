b710d5951b98b11ac9c17588b4e5f3032d503334
iZeQure <TobiasRosenvinge@hotmail.com>
2021-03-06 22:24:36 +0100

    Api Changes v3.2.1
    Removed unused dependencies.

--- TQC-Development/ApiEmerald/Startup.cs
+++ TQC-Development/ApiEmerald/Startup.cs
@@ -1,5 +1,3 @@
-using DatabaseAccess.Database;
-using DatabaseAccess.Database.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
